30f6c24e1cc4125b4d8ea06519004d5e6167b5be
Mauro Martinez <maurohmartinez@gmail.com>
2023-04-12 16:27:42 +0300

    Route fixed

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,4 +3,4 @@
 use DigitalPulse\KeepMeSynced\app\Http\Controllers\KeepMeSyncedController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('keep-me-synced/hook', [KeepMeSyncedController::class, 'hook']);
+Route::post('keep-me-synced/hook', [KeepMeSyncedController::class, 'hook'])->middleware('web');
